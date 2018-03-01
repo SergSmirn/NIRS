9d8f98108eea9541e09fd0f4767fa9644eb4d9ec
Sergey Smirnov <kurchatov1996@yandex.ru>
2018-03-01 22:02:43 +0300

    hz

--- NIRS/settings.py
+++ NIRS/settings.py
@@ -90,12 +90,8 @@ WSGI_APPLICATION = 'NIRS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django',
-        'USER': 'serega',
-        'PASSWORD': '0',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
